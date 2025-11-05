a8af821e362876228e66768260698dbf9d3ad35f
Rukhsar Bano <afsanabegum6430@gmail.com>
2025-11-05 12:45:48 +0530

    update

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const user = require("../models/user.js");
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
 
-router.post("/signup", async(req, res) => {
-    let {username, email, password} = req.body;
+router.post("/signup", wrapAsync (async(req, res) => {
+    try{
+        let {username, email, password} = req.body;
     const newUser = new User({email, username});
     const registeredUser = await User.register(newUser, password);
     console.log(registeredUser);
     req.flash("success", "Welcome to LuxeLay!");
+    res.redirect("/listings");
+    } catch(e){
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    };
+}));
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post("/login", passport.authenticate("local", {failureRedirect: '/login', failureFlash: true}), async(req, res) => {
+    req.flash("success", "Welcome to LuxeLay! You are logged in!");
     res.redirect("/listings");
 });
 
